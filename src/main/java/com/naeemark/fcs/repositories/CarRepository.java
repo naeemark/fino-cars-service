ce6c1906d7c5a9cf1cb099d411347e3795027ea4
Naeem <naeemark@gmail.com>
2023-03-28 00:12:16 +0200

    Completes flow for the functional requirements

--- src/main/java/com/naeemark/fcs/repositories/CarRepository.java
+++ src/main/java/com/naeemark/fcs/repositories/CarRepository.java
@@ -1,7 +1,7 @@
 package com.naeemark.fcs.repositories;
 
 import com.naeemark.fcs.models.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,5 +10,6 @@ import org.springframework.stereotype.Component;
  * Created on: 2023-03-27
  */
 @Component
-public interface CarRepository extends JpaRepository<Car, Integer> {
+public interface CarRepository extends PagingAndSortingRepository<Car, Integer> {
+    Iterable<Car> findByIdOrMakeOrModelYearOrYearOrTypeOrFuelType(Integer id, String make, String modelYear, Integer year, String type, String fuelType);
 }
--- src/main/java/com/naeemark/fcs/services/CarServiceImpl.java
+++ src/main/java/com/naeemark/fcs/services/CarServiceImpl.java
@@ -2,17 +2,26 @@ package com.naeemark.fcs.services;
 
 import com.naeemark.fcs.models.Car;
 import com.naeemark.fcs.repositories.CarRepository;
+import lombok.Generated;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import static com.naeemark.fcs.utils.Constants.ERROR_DATA_NOT_FOUND;
+import static com.naeemark.fcs.utils.Constants.ERROR_PARAM_NOT_ALLOWED;
 
 
 /**
@@ -32,10 +41,51 @@ public class CarServiceImpl implements CarService {
     @Override
     public List<Car> findAll() {
         try {
-            return carRepository.findAll();
+            Iterable<Car> all = carRepository.findAll();
+            return StreamSupport.stream(all.spliterator(), false).collect(Collectors.toList());
         } catch (Exception e) {
             logger.error(e.getMessage());
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, ERROR_DATA_NOT_FOUND);
+        }
+    }
+
+    @Override
+    public List<Car> findPage(int pageNo, int pageSize, String sortBy) {
+        try {
+            Pageable pageRequest = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
+            Iterable<Car> all = carRepository.findAll(pageRequest);
+            return StreamSupport.stream(all.spliterator(), false).collect(Collectors.toList());
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ERROR_DATA_NOT_FOUND);
+        }
+    }
+
+    @Generated
+    @Override
+    public List<Car> search(Integer id, String make, String modelYear, Integer year, String type, String fuelType) {
+        try {
+            validateParams(id, make, modelYear, year, type, fuelType);
+            Iterable<Car> search = carRepository.findByIdOrMakeOrModelYearOrYearOrTypeOrFuelType(id, make, modelYear, year, type, fuelType);
+            return StreamSupport.stream(search.spliterator(), false).collect(Collectors.toList());
+        } catch (IllegalArgumentException e) {
+            logger.error(e.getMessage());
+            throw new ResponseStatusException(HttpStatus.METHOD_NOT_ALLOWED, ERROR_PARAM_NOT_ALLOWED);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ERROR_DATA_NOT_FOUND);
+        }
+    }
+
+    /**
+     * Utility Method to validate provide number of search parameters
+     * @param params Array
+     */
+    @Generated
+    private void validateParams(Object... params) {
+        long count = Arrays.stream(params).filter(Objects::nonNull).count();
+        if (count > 1) {
+            throw new IllegalArgumentException(ERROR_PARAM_NOT_ALLOWED);
         }
     }
 }
