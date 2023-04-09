14cb7081ab708ecfe2f2caaec7fcf3a1385785a2
Naeem <naeemark@gmail.com>
2023-04-09 20:58:07 +0200

    Enable CORS

---
+++ src/main/java/com/naeemark/fcs/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.naeemark.fcs.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Naeem <naeemark@gmail.com>.
+ * <p>
+ * Created on: 2023-04-05
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
